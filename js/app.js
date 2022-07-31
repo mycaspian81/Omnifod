9b5fccac00c19d03bb55ff7901e4ed0ea3e4c7f5
amirhossein gharebaghi <mycaspian81@gmail.com>
2022-07-31 05:57:22 +0430

    finished the all

--- js/app.js
+++ js/app.js
@@ -14,3 +14,54 @@ btnOpenNavEl.addEventListener('click', function() {
 btnCloseNavEl.addEventListener('click', function() {
     headerEl.classList.toggle('nav__open');
 })
+
+////////////////////////////////
+//smooth scroling
+////////////////////////////////
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const href = link.getAttribute("href");
+
+        //scroll to top
+        if (href === "#") 
+        window.scrollTo ({
+            top:0,
+            behavior: "smooth",
+        })
+        //scroll to another link
+        if (href !== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({ behavior: "smooth" });
+        }
+
+        //remove navbar
+        if (link.classList.contains("main__nav")) {
+            headerEl.classList.toggle('nav__open');
+        }
+    });
+});
+
+/////////////////////////////
+//sticky nav
+const sectionHeroEl = document.querySelector(".header__main");
+const obs = new IntersectionObserver(function(entries) {
+const ent = entries[0];
+if (ent.isIntersecting === false)
+    document.body.classList.add("sticky")
+    if (ent.isIntersecting === true)
+    document.body.classList.remove("sticky")
+}, 
+{
+    root: null,
+    threshold:0,
+    rootMargin:"-80px"
+});
+obs.observe(sectionHeroEl);
+
+
+/////////////////////////////
